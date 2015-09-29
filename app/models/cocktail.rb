9d549b0a5bb27087fc9b538f05f58ad2aa14b931
Gundars Ostrovskis <gundarso@gmail.com>
2015-09-29 12:52:55 +0200

    still struggling

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,13 +1,9 @@
 class Cocktail < ActiveRecord::Base
-  attr_accessor :picture_file_name
-
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
-
   has_attached_file :picture,
     styles: { medium: "300x300>", thumb: "100x100>" }
-
   validates_attachment_content_type :picture,
     content_type: /\Aimage\/.*\z/
 end
