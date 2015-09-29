ae34eef13491c7e1635a228522fe580bf8ad9475
Gundars Ostrovskis <gundarso@gmail.com>
2015-09-29 12:56:31 +0200

    added attr accessor for picture

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,4 +1,6 @@
 class Cocktail < ActiveRecord::Base
+  attr_accessor :picture_file_name
+
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
